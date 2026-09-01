8129dd53d156665859eaae69eebed19cf5c5fc56
sinalizaappsst <sinaliza.app.sst@gmail.com>
2026-09-01 16:49:26 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-// Service worker desativado nesta versão para evitar cache antigo.
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
